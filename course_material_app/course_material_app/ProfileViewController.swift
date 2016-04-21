524801a5e5863ea768a1fc1d46fac55770804c6f
xiangormirko <xiangormirko@gmail.com>
2016-04-22 00:05:35 +0800

    initial structure

---
+++ course_material_app/course_material_app/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  course_material_app
+//
+//  Created by MIRKO on 4/21/16.
+//  Copyright © 2016 hku_project. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
