b9722affe4067dcf3662d5ecc651e67b95d3a2b8
Robert <roberwei1989@gmail.com>
2016-04-30 14:26:12 +0800

    finish sign in and merge story board
    finish sign in and merge story board

--- course_material_app/course_material_app/SignUpViewController.swift
+++ course_material_app/course_material_app/SignUpViewController.swift
@@ -2,7 +2,7 @@
 //  SignUpViewController.swift
 //  course_material_app
 //
-//  Created by MIRKO on 4/21/16.
+//  Created by Robert Wei on 4/21/16.
 //  Copyright © 2016 hku_project. All rights reserved.
 //
 
@@ -12,7 +12,7 @@ import Foundation
 import UIKit
 
 class SignUpViewController: UIViewController {
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
